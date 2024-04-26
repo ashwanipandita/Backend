50ec6c86a33f869225fef0377c07cda5ddd6558e
Ashwani Pandita <ashwanipandita97@gmail.com>
2024-04-27 04:01:18 +0530

    update

--- Day-12-Auth/controllers/auth.controllers.js
+++ Day-12-Auth/controllers/auth.controllers.js
@@ -2,27 +2,31 @@ import UserSchema from "../models/user.schema.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const Register = async(req,res) => {
-
-try{
-
-    const {name, email, password, confirmPassword } = req.body.userData;
-    if (!name || !email || !password || !confirmPassword){
-        return res.json ({success : false , message : "All fields are required."});
+export const Register = async (req, res) => {
+  try {
+    const { name, email, password, confirmPassword } = req.body.userData;
+    if (!name || !email || !password || !confirmPassword) {
+      return res.json({ success: false, message: "All fields are required." });
     }
-if(password !== confirmPassword){
-    return res.json ({success : false , message : "Password & ConfirmPassword not matched"});  
-}
+    if (password !== confirmPassword) {
+      return res.json({
+        success: false,
+        message: "Password and Confirm is not matched.",
+      });
+    }
 
-const isEmailExists = await UserSchema.findOne({email:email});
-// console.log(isEmailExists,"isEmailExists");
-if(isEmailExists){
-    return res.json({success : false, message : "Email already exist,Please use another email id"});
-}
+    const isEmailExists = await UserSchema.findOne({ email: email });
+    // console.log(isEmailExists, "isEmailExists");
+    if (isEmailExists) {
+      return res.json({
+        success: false,
+        message: "Email is alreadly exist, Please use another one.",
+      });
+    }
 
-const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-// 1st type to store data in mongodb
+    // 1st type to store data in mongodb
     // const newUser = await UserSchema.create({
     //   name: name,
     //   email: email,
@@ -31,60 +35,65 @@ const hashedPassword = await bcrypt.hash(password,10);
 
     // 2nd type to store data in mongodb
     const newUser = new UserSchema({
-        name: name,
-        email: email,
-        password: hashedPassword,
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return res.json({ success: true, message: "Registeration Completed." });
+  } catch (error) {
+    console.log(error, "error");
+    return res.json({ error, success: false });
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const { email, password } = req.body.userData;
+    if (!email || !password) {
+      return res.json({ success: false, message: "All fields are required." });
+    }
+
+    const user = await UserSchema.findOne({ email: email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not exist, Please check your email.",
       });
+    }
 
-await newUser.save();
+    // console.log(user, "user");
 
-return res.json({success : true , message : "Registration Completed."});
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-}catch(error){
-    console.log(error,"error");
-    return res.json({error, success : false });
-}
-
+    if (!isPasswordCorrect) {
+      return res.json({
+        success: false,
+        message: "Password is wrong.",
+      });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    console.log(token, "token");
+    // token -> cookie -> localStorage, cookies
+    // userData  -> context -> context, redux
+    // compare user password with stored password in db
+    res.cookie("token", token);
+    return res.json({
+      success: true,
+      message: "Login Successfull.",
+      userData: user,
+    });
+  } catch (error) {
+    console.log(error, "error");
+    return res.json({ error, success: false });
+  }
 };
 
-export const Login = async (req,res) => {
-    try{
-        const{ email, password } = req.body.userData;
-        if (!email || !password) {
-            return res.json({ success: false, message: "All fields are required." });
-          }
 
-          const user = await UserSchema.findOne({ email: email });
-          if (!user) {
-            return res.json({
-              success: false,
-              message: "User does not exist, Please check your email.",
-            });
-          }
-      
-   console.log(user, "user");
-
-   const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-   if (!isPasswordCorrect) {
-     return res.json({
-       success: false,
-       message: "Password is wrong.",
-     });
-   }
-   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-   console.log(token, "token");
-   // token -> cookie -> localStorage, cookies
-   // userData  -> context -> context, redux
-   // compare user password with stored password in db
-   res.cookie("token", token);
-   return res.json({
-     success: true,
-     message: "Login Successfull.",
-     userData: user,
-   });
- } catch (error) {
-   console.log(error, "error");
-   return res.json({ error, success: false });
- }
-};
+// 1. create controller for validate-token 
+// 2. access token from cookie 
+// 3. decrypt token by using jwt, id
+// 4. use id to find user from mongodb findById
+// 5.response , userDate, 
--- Day-12-Auth/index.js
+++ Day-12-Auth/index.js
@@ -1,32 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import AllRoutes from './routes/index.js';
+import AllRoutes from "./routes/index.js";
 import cors from "cors";
 
 var corsOptions = {
-    origin: "http://localhost:3000",
-    credentials: true,
-  };
-
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 const app = express();
 app.use(express.json());
 dotenv.config();
-app.use(cors());
+app.use(cors(corsOptions));
 
-
-
-app.get('/',(req,res)=>{
-    res.send("Working..");
+app.get("/", (req, res) => {
+  res.send("Working..");
 });
 
-app.use('/api/v1', AllRoutes)
+app.use("/api/v1", AllRoutes);
 
-mongoose.connect(process.env.MONGODB_URL).then(()=>{
-    console.log("DB Connected.")
-})
-
-app.listen(3001, ()=>{
-    console.log("Listening on port 3001.")
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("DB connected.");
 });
 
+app.listen(3001, () => {
+  console.log("Listening on port 3001.");
+});
