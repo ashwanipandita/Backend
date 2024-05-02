a0d0d6a290dfe0454dc20a78b18158a8ae44fab3
Ashwani Pandita <ashwanipandita97@gmail.com>
2024-05-02 14:30:19 +0530

    update

---
+++ Day-15/schemas/product.schema.js
@@ -0,0 +1,15 @@
+import mongoose,{Schema} from "mongoose";
+
+const productSchema = new Schema({
+name : String,
+category : String,
+price: Number,
+quantity : Number,
+});
+
+const ProductSchema = mongoose.model("Product",productSchema);
+
+
+export default ProductSchema;
+
+
